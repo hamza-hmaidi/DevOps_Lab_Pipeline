adb603ddc85e9e8f70875ed68ae7012b3aada341
hamza-hmaidi <hamza.hmaidi@insat.u-carthage.tn>
2022-04-29 12:59:53 +0100

    change

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World hamza!')
+  res.send('hello hamza')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+module.exports = app;
